5342a112093d6c94c2819dcd51a90951e8348102
Raziel <chokokokyunokibou@gmail.com>
2023-05-13 21:50:31 -0400

    Commiting changes before computer dies again.

--- network/network.cpp
+++ network/network.cpp
@@ -12,6 +12,15 @@ class SingleLayerNeuralNetwork {
 	    learningRate = learningRateInitializer;
         } 
 
+    std::vector<int> predict(std::vector<double> input) {
+            std::vector<int> networkOutput;
+            for(Neuron neuron:neurons) {
+                int neuronOutput = neuron.predict(input);
+                networkOutput.push_back(neuronOutput);
+            }
+            return networkOutput;
+        }
+
     private:
 	std::vector<Neuron> neurons;
 	double learningRate;
--- test.cpp
+++ test.cpp
@@ -1,13 +1,39 @@
 #include <vector>
 #include <iostream>
 #include "vector-operations/vector-operations.hpp"
-#include "neuron/neuron.cpp"
+#include "network/network.cpp"
+
+
+void printVector(std::vector<double> vec);
+void printVector(std::vector<int> vec);
 
 int main() {
-    std::vector<double> a {1,2.2,10,2};
-    std::vector<double> b {10,10,10,1};
-    double c = dot_product(a,b);
-    std::cout << c << "\n";
-    Neuron neuron = Neuron(a,-134);
-    std::cout << neuron.predict(b) << "\n";
+    /*
+        std::vector<double> b {10,10,10,1};
+        double c = dot_product(a,b);
+        std::cout << c << "\n";
+        Neuron neuron = Neuron(a,-134);
+        std::cout << neuron.predict(b) << "\n";
+    */
+    SingleLayerNeuralNetwork nn = SingleLayerNeuralNetwork(3*3, 5, 0.5);    
+    std::vector<double> input {1,1,1,0,1,0,0,1,0};
+    std::vector<int> output = nn.predict(input); 
+    std::cout << "Input to Neural Network: " << printVector(input); 
+    std::cout << "Output: " << printVector(output);    
+}
+
+void printVector(std::vector<int> vec) {
+    std::cout << "[ ";
+    for (int num : vec) {
+        std::cout << num << " ";    
+    }
+    std::cout << " ]\n";
+}
+
+void printVector(std::vector<double> vec) {
+    std::cout << "[ ";
+    for (double num : vec) {
+        std::cout << num << " ";    
+    }
+    std::cout << " ]\n";
 }
